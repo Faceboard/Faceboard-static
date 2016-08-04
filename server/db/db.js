0739e7be2b5e04e53d33a4f8409530fe15cd5516
Jason Bland <jtbland@gmail.com>
2016-08-04 15:09:08 -0700

    (feat) add db.js and package.json libraries

---
+++ server/db/db.js
@@ -0,0 +1,10 @@
+var Sequelize = require('sequelize');
+var settings = require('./settings.js');
+
+var url = process.env.DATABASE_URL || settings.DBURL;
+
+var options = process.env.DATABASE_URL ? { "ssl": true, "dialectOptions": { "ssl": { "require": true }}} : { logging: false };
+
+var sequelize = new Sequelize( url, options );
+
+module.exports = sequelize;
